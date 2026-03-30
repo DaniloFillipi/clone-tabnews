572c032116fcd36ec350a018aadb01d20f0b67da
Danilo Fillipi <149070803+DaniloFillipi@users.noreply.github.com>
2026-03-30 20:50:53 +0000

    Adiciona versão, conexões e conexões abertas na pagina status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,8 +3,23 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
+  const versionDbResult = (
+    await database.query("SELECT current_setting('server_version') AS version;")
+  ).rows[0].version;
+  const maxConnectionsResult = (
+    await database.query("SELECT current_setting('max_connections') AS max;")
+  ).rows[0].max;
+  const openedConnectionsResult = (
+    await database.query(
+      "SELECT count(*) AS opened FROM pg_stat_activity;",
+    )
+  ).rows[0].opened;
+
   response.status(200).json({
     updated_at: updatedAt,
+    version_postgres: versionDbResult,
+    max_connections: maxConnectionsResult,
+    opened_connections: openedConnectionsResult,
   });
 }
 
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -4,8 +4,19 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version_postgres).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.opened_connections).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
+  const versionDbResult = responseBody.version_postgres;
+  expect(responseBody.version_postgres).toEqual(versionDbResult);
+
+  const maxConnectionsResult = responseBody.max_connections;
+  expect(responseBody.max_connections).toEqual(maxConnectionsResult);
+
+  const openedConnectionsResult = responseBody.opened_connections;
+  expect(responseBody.opened_connections).toEqual(openedConnectionsResult);
 });
